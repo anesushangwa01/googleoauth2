ba6e523ad6f31decb66adc2ebf37dbc9d86fe376
Anesu shangwa <97034450+Anesushangwa@users.noreply.github.com>
2024-10-31 14:39:05 +0200

    new

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ dotenv.config();
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGO_URI);
+
 
 app.use(session({
   secret: 'secret',
